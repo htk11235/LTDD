018c13928f0a5792f961ddd702563a21754fcc44
huynh trong khoa <htk11235@gmail.com>
2022-09-12 19:20:23 +0700

    up-project

---
+++ app/src/main/java/com/example/tuan2_profile_huynhtrongkhoa_2050531200216/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tuan2_profile_huynhtrongkhoa_2050531200216;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
